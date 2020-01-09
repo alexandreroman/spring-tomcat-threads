636acbe4d802c7010cfaa4ecedb2bfa4181884c5
Alexandre Roman <aroman@pivotal.io>
2020-01-09 12:13:36 +0100

    Expose Tomcat connection queue metrics

--- src/main/java/fr/alexandreroman/demos/springtomcatthreads/Application.java
+++ src/main/java/fr/alexandreroman/demos/springtomcatthreads/Application.java
@@ -18,19 +18,28 @@ package fr.alexandreroman.demos.springtomcatthreads;
 
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.binder.BaseUnits;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class Application {
@@ -72,18 +81,56 @@ class SystemInfoController {
 }
 
 @Configuration
+@Slf4j
 class MetricsConfig {
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     @Bean
-    Gauge runnableThreadCount(MeterRegistry registry) {
+    Gauge runnableThreadCount() {
         return Gauge.builder("jvm.threads.runnable", this::computeRunnableThreadCount)
-                .baseUnit("threads")
+                .baseUnit(BaseUnits.THREADS)
                 .description("The number of threads in state RUNNABLE")
-                .register(registry);
+                .register(meterRegistry);
     }
 
     private Number computeRunnableThreadCount() {
         return Thread.getAllStackTraces().keySet().stream()
                 .filter(t -> t.getState().equals(Thread.State.RUNNABLE))
                 .count();
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    void registerAdditionalTomcatMetrics() throws MalformedObjectNameException {
+        log.info("Registering additional Tomcat metrics");
+
+        final var mbeanServer = ManagementFactory.getPlatformMBeanServer();
+        final var jmxDomain = "Tomcat";
+        final var threadPoolKey = jmxDomain + ":type=ThreadPool,name=*";
+        final var threadPoolObjectNames = mbeanServer.queryNames(new ObjectName(threadPoolKey), null);
+
+        for (final var threadPoolObjectName : threadPoolObjectNames) {
+            final var tags = Tags.of("name", threadPoolObjectName.getKeyProperty("name"));
+            registerGauge(meterRegistry, "tomcat.threads.connectionCount", "connections", tags, () -> {
+                return getDouble(mbeanServer, threadPoolObjectName, "connectionCount");
+            });
+            registerGauge(meterRegistry, "tomcat.threads.maxConnections", "connections", tags, () -> {
+                return getDouble(mbeanServer, threadPoolObjectName, "maxConnections");
+            });
+        }
+    }
+
+    private static void registerGauge(MeterRegistry meterRegistry,
+                                      String name, String units, Tags tags, Supplier<Number> source) {
+        Gauge.builder(name, source).baseUnit(units).tags(tags)
+                .register(meterRegistry);
+    }
+
+    private static Double getDouble(MBeanServer server, ObjectName objectName, String attribute) {
+        try {
+            return Double.parseDouble(server.getAttribute(objectName, attribute).toString());
+        } catch (Exception e) {
+            return Double.NaN;
+        }
     }
 }
